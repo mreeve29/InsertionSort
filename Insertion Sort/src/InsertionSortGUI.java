6f8077840cf50cbd0955302680d691fe54d5a990
Michael Reeve <michael.s.reeve@gmail.com>
2020-01-31 09:12:38 -0500

    generics

--- Insertion Sort/src/InsertionSortGUI.java
+++ Insertion Sort/src/InsertionSortGUI.java
@@ -3,29 +3,23 @@ import java.util.Arrays;
 
 import BreezySwing.GBFrame;
 
-public class InsertionSortGUI extends GBFrame{
+public class InsertionSortGUI extends GBFrame {
 
-	
-	
 	public static void main(String[] args) {
-		int ar[] = {-3, 1, -4, 10, 3, 8, 5};
 		ArrayList<Integer> arr = new ArrayList<Integer>();
-		
+
 		arr.add(-1);
 		arr.add(0);
 		arr.add(13);
 		arr.add(8);
 		arr.add(5);
+
+		Sorter<Integer> s = new Sorter<Integer>(arr);
 		
-		Sorter s = new Sorter();
-		
-		arr = s.realSort(arr);
-		
-		for(Integer i : arr) {
+		for(Integer i : s.getList()) {
 			System.out.println(i);
 		}
-		
-		
+
 
 	}
 
--- Insertion Sort/src/Sorter.java
+++ Insertion Sort/src/Sorter.java
@@ -1,45 +1,29 @@
 import java.util.ArrayList;
 
-public class Sorter {
-	private ArrayList<Integer> sorted;
+public class Sorter<T extends Comparable<T>> {
+	private ArrayList<T> sorted;
 	
-	public Sorter() {
-		sorted = new ArrayList<Integer>();
+	public Sorter(ArrayList<T> arr) {
+		sorted = arr;
+		sort();
 	}
 	
-	public ArrayList<Integer> sort(ArrayList<Integer> arr) {
-		for(int i = 1; i < arr.size(); i++) {
-			int num = arr.get(i);
+	public void sort() {
+		for(int i = 1; i < sorted.size(); i++) {
+			T num = sorted.get(i);
 			int pos = i;
 			
-			while(pos > 0 && arr.get(pos-1) > num) {
-				arr.set(pos, arr.get(pos-1));
+			while(pos > 0 && sorted.get(pos-1).compareTo(num) > 0) {
+				sorted.set(pos, sorted.get(pos-1));
 				pos--;
 			}
 			
-			arr.set(pos, num);
-			
-			
+			sorted.set(pos, num);	
 		}
-		return arr;
-		
 	}
 	
-	public ArrayList<Integer> realSort(ArrayList<Integer> arr){
-		ArrayList<Integer> sorted = new ArrayList<Integer>();
-		if(arr.size() == 0)return sorted;
-		sorted.add(arr.get(0));
-		
-		for(int i = 1; i < arr.size(); i++) {
-			int num = arr.get(i);
-			
-		}
-		
-		
+	public ArrayList<T> getList(){
 		return sorted;
 	}
-	
-	
-	
 	
 }
